975e2636f52ba7073c9a747427b1a9333e47e43a
John Noll <john.noll@lero.ie>
2013-09-21 12:59:21 +0100

    Added a bunch of ui-related scripts.

--- data/static/js/create.js
+++ data/static/js/create.js
@@ -1,12 +1,38 @@
 
-$(document).ready(function(){
-	// createPageSubmit is the submit button on the "Create" dialog in the left pane.
-	$("#createPageSubmit").click(function(e){
-		e.preventDefault();
-		$("#editedText").attr({value: $("#pageType").val()});
-		$("#createPageForm").attr({action: "/_edit/" + $("#pageName").val()});
-		console.log("create.js: submitting" + $("#createPageForm").attr("action"));
 
-		$("#createPageForm").submit();
-	    });
+$(document).ready(function(){
+    // createPageSubmit is the submit button on the "Create" dialog in the left pane.
+    $("#createPageSubmit").click(function(e){
+	e.preventDefault();
+	$("#editedText").attr({value: $("#pageType").val()});
+	$("#createPageForm").attr({action: "/_edit/" + $("#pageName").val()});
+	console.log("create.js: submitting" + $("#createPageForm").attr("action"));
+
+	$("#createPageForm").submit();
     });
+
+    $.getScript("/js/jquery.dataTables.min.js")
+	.done(function(script, textStatus){
+	    console.log("jquery.dataTables.min.js: " + textStatus);
+	    $.getScript("/js/forms.js")
+		.done(function(script, textStatus){
+		    console.log("forms.js: " + textStatus);
+		})
+		.fail(function(jqxhr, settings, exception){
+		    console.log("load forms.js failed");
+		});
+
+	})
+	.fail(function(jqxhr, settings, exception){
+	    console.log("load jquery.dataTables.min..js failed");
+	});
+
+    $.getScript("/js/consult.js")
+	.done(function(script, textStatus){
+	    console.log("consult.js: " + textStatus);
+	    submitConsult();
+	})
+	.fail(function(jqxhr, settings, exception){
+	    console.log("load consult.js failed");
+	});
+});
